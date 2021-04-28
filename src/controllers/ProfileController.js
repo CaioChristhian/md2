894189070d5a971805d143bc79041b80b157850d
Caio Christhian <caiochristhianz@gmail.com>
2021-04-28 11:08:36 -0300

    second commit

---
+++ src/controllers/ProfileController.js
@@ -0,0 +1,39 @@
+const Profile = require('../model/Profile')
+
+
+module.exports = {
+    async index(req, res) {
+        return res.render("profile", { profile: await Profile.get() })
+    },
+
+    async update(req, res) {
+        // req.body para pegar os dados
+        const data = req.body
+
+        // definir quantas semanas tem em um ano: 52
+        const weeksPerYear = 52
+
+        // remover as semanas de férias do ano, para pegar quantas semanas tem em 1 mês
+        const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+
+        // total de horas trabalhadas na semana
+        const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+        // total de horas trabalhadas no mes
+        const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+        // qual será o valor da minha hora
+        const valueHour = data["monthly-budget"] / monthlyTotalHours
+
+        const profile = await Profile.get()
+
+
+        await Profile.update({
+            ...profile,
+            ...req.body,
+            "value-hour": valueHour
+        })
+        
+        return res.redirect('/profile')
+    },
+}
